f500b81adc3731f014b82ee795c8b2881704eee9
HermanLeeJunpeng <104064498+HermanLeeJunpeng@users.noreply.github.com>
2022-04-21 19:54:20 +0800

    Mode error

--- Question5.java
+++ Question5.java
@@ -1,32 +1,48 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Question5
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
+    int UserInput, IndividualNum;
+    int maxcount = 0;
+    int maxvalue = 0;
+
+    ArrayList<Integer> array = new ArrayList<>();
+    System.out.println("Please enter the iterations of number to input"); 
     Scanner in = new Scanner(System.in);
-    
+    UserInput = in.nextInt();
+
+    for(int i = 0; i < UserInput; i++)
+      {
+        System.out.print("Enter a number: ");
+        IndividualNum = in.nextInt();
+        array.add(IndividualNum);
+      }
+
+      int ArraySize = array.size();
+      int mode[] = new int[ArraySize];
+      for (int i =0; i < mode.length; i++)
+      {
+        int currentvalue = mode[i];
+        int currentcount = 1;
+
+        for (int j = i + 1; j < mode.length; ++j)
+          {
+            ++currentcount;
+          }
+
+        if (currentcount > maxcount)
+        {
+          maxcount = currentcount;
+          maxvalue = currentvalue;
+        }
+        else if (currentcount == maxcount)
+          {
+            maxvalue = maxvalue;
+          }
+      }  
+      System.out.println("mode: " + maxvalue);
   }
 }
