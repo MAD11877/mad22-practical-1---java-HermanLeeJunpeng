c2d414a9a81d4215e74eebfab8bd5b00280608cd
HermanLeeJunpeng <104064498+HermanLeeJunpeng@users.noreply.github.com>
2022-04-21 16:46:38 +0800

    BMI Calculator done

--- Question2.java
+++ Question2.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Question2
 {
   public static void main(String[] args)
-  {
-    /**
-     * Prompts user for height and weight, calculate and print out the BMI of the user
-     * 
-     * Hint: in.nextDouble() to read double
-     */
-     
+  {    
     Scanner in = new Scanner(System.in);
+    System.out.println("Please enter your height");
     double height = in.nextDouble();
+    System.out.println("Please enter your weight in KG");
+    double weight = in.nextDouble();
+
+    double BMI = weight / (height * height);
+    System.out.println("Your Bmi is: " + BMI);
   }
 }
